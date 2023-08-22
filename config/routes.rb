eda0ca3b86fa4f00258f3e1d15650cde103cf710
Millene Prado <m.prado@researchable.nl>
2023-08-22 11:13:16 -0300

    finish CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # verb 'path', to: 'controller#action'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # see all restaurants
+  get 'restaurants', to: 'restaurants#index'
+
+  # create a restaurant
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
+
+  # see one restaurant
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # update a restaurant
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  patch 'restaurants/:id', to: 'restaurants#update'
+
+  # delete a restaurant
+  delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  # resources :restaurants
+  # resources :restaurants, only: [:index]
+  # resources :restaurants, except: [:index, :edit, :update]
 end
